e7700836dedc72fd048b175c627414629b22a784
Fox <m.starostina@pflb.ru>
2016-04-26 19:11:17 +0300

    Вывод на экран "запуск программы"
    Вывод на экран "завершение программы"

--- src/Bank.java
+++ src/Bank.java
@@ -5,6 +5,9 @@ public class Bank {
 
     public void receivesInformationFromTheEmployee() {
         System.out.println("Банк получает информацию от сотрудника");
+        Finish back = new Finish();
+        back.finishProgramm();
+
 
 
     }
